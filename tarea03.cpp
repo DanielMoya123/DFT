e47dd4e01d6af37f04239af6888c326310eb7a6d
DanielMoya123 <danielmscr@hotmail.com>
2017-03-25 07:48:15 -0600

    Partial code

--- tarea03.cpp
+++ tarea03.cpp
@@ -6,8 +6,10 @@
 #include <cstdlib>
 #include <stdexcept>
 
-#include <ltiTimer.h>
 
+#include <ltiTimer.h>
+#include <ltiGaussKernels.h>
+#include <ltiBinaryKernels.h>
 
 
 /******************************************************
@@ -31,14 +33,54 @@ int main(int argc, char *argv[])
 
 	lti::timer chron;
 
-      chron.start();
+    chron.start();
+
+
+    lti::loadImage loader; // functor to load images
+	lti::image img;        // the image loaded will be left here.
+
+	loader.load("Lenna.png",img);  // load PNG image
+
+	//// CONVERSION FROM img TO channel IS MISSING !!
+
+	lti::channel  imgGray;
+
+	double variance;
+
+	// kSize NEEDS TO BE DEFINED AS AN ENTRY PARAMETER
+	variance = pow((kSize+2)/6, 2)
+
+	//Guassian kernel
+	lti::gaussKernel2D<lti::channel::value_type> gKernel(kSize,variance); 
+ 	lti::convolution gFilter;                        // convolution operator
+ 	lti::convolution::parameters gParam;             // parameters
+ 	gParam.setKernel(gKernel);                        // use the gauss kernel
+ 	gFilter.setParameters(gParam);                    // set parameters
+	gFilter.apply(imgGray);
+
+	//Octogonal kernel
+	lti::octagonalKernel<lti::channel::value_type> oKernel(kSize,variance); 
+ 	lti::convolution oFilter;                        // convolution operator
+ 	lti::convolution::parameters oParam;             // parameters
+ 	oParam.setKernel(oKernel);                        // use the ortogonal kernel
+ 	oFilter.setParameters(oParam);                    // set parameters
+	oFilter.apply(imgGray);
+
+
+
+
+
+
+
+	
+
 
       
 
-      chron.stop();
+    chron.stop();
 
 
-      cout << "Duration: " << chron.getTime() << " microseconds\n";
+    cout << "Duration: " << chron.getTime() << " microseconds\n";
 
 	return EXIT_SUCCESS;
   

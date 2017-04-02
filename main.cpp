8e50ce3d653dd76eac3740469aa02a6c3c7ece8d
DanielMoya123 <danielmscr@hotmail.com>
2017-04-01 18:54:49 -0600

    Code for running in 2D and octogonal code

--- main.cpp
+++ main.cpp
@@ -29,12 +29,17 @@ using namespace std;
  * Definitions
  * 
  * ****************************/
-#define KMAX 1023
-#define KMIN 3
+#define K_MAX 1023
+#define K_MIN 3
 #define N_MIN 64
 #define N_MAX 1920
 #define M_MIN 48
 #define M_MAX 1080
+#define K_STEP 120
+#define N_STEP 185
+#define M_STEP 103
+#define NC_MIN 10
+#define NC_MAX 100
  
 
 /******************************************************
@@ -72,39 +77,55 @@ int main(int argc, char *argv[])
 
 			dataMatrix[i] = new double[10];
 		
-		int u = 100, v = 100;
-		int kSize = 3;
+		//int u = 100, v = 100;
+		//int kSize = 3;
 		
-		lti::matrix<float> img(u,v);
+		lti::matrix<float> img(N_MAX,M_MAX);
 		
-		for (int i = 0; i < u; i++)
+		for (int i = 0; i < M_MAX; i++)
 		{
-			for (int j = 0; j < v; j++)
+			for (int j = 0; j < N_MAX; j++)
 			{
 				img.at(i,j) = (int)(((float)rand()/(float)RAND_MAX)*255);
 			}
 		}
 		
 		// We get the size in x or y of the filter and set the kSize
-		double variance = pow((kSize+2)/6, 2);
+		double variance = 0;
+		
 		
 		// *************************************  
 		// We create the filter
 		// ************************************* 
 		FilterImage filterController = FilterImage();
 		
-		// We create the 2D kernel 
-		lti::kernel2D<float> dKernel = filterController.GenerateSquareOddGaussianFilter(kSize,variance,false);
-		// We create the octogonal kernel 
-		lti::kernel2D<float> gKernel2D = filterController.GenerateSquareOddGaussianFilter(kSize,variance,true);
+
+		int kSize = K_MAX;
+		
+		
 		
 		int n = 0;
+		int n_actual = 0;
+		int m_actual = 0;
 
+		lti::matrix<float> imgD(N_MAX,M_MAX);
+		/******************************
+		2D FILTER LOOP
+		*******************************/
 		for (int i = 0; i < 10; i++)
 		{
+			n_actual = N_MAX-i*N_STEP;
+			m_actual = M_MAX-i*M_STEP;
+			imgD = img.copy(imgD, 0, n_actual, 0, m_actual);
 			for (int j = 0; j < 10; j++)
 			{
-				
+				kSize -= K_STEP*j;
+				variance = pow((kSize+2)/6, 2);
+				// We create the 2D kernel 
+				lti::kernel2D<float> dKernel = filterController.GenerateSquareOddGaussianFilter(kSize,variance,false);
+
+				n = ( 1-( (n_actual*m_actual + kSize*kSize)/(N_MAX*M_MAX + K_MAX*K_MAX) ) ) * (NC_MAX - NC_MIN) + NC_MIN;
+
 				// Calculate the new image and filter to get the n data
 				for (int k = 0; k < n; k++)
 				{
@@ -112,13 +133,55 @@ int main(int argc, char *argv[])
 					chron.start();
 		
 					// Funciones para tomar el tiempo
-					filterController.ConvolutionSquareFilter(dKernel,img);
+					filterController.ConvolutionSquareFilter(dKernel,imgD);
 
 					chron.stop();
-					dataMatrix[i][j] =  (dataMatrix[i][j] + chron.getTime())/2;
+					dataMatrix[i][j] =  (dataMatrix[i][j] + chron.getTime())/n;
 				}
 			}
 		}
+
+
+
+		lti::matrix<float> imgG(N_MAX,M_MAX);
+		/******************************
+		OCTOGONAL LOOP
+		*******************************/
+		for (int i = 0; i < 10; i++)
+		{
+			n_actual = N_MAX-i*N_STEP;
+			m_actual = M_MAX-i*M_STEP;
+			imgG = img.copy(imgG, 0, n_actual, 0, m_actual);
+			for (int j = 0; j < 10; j++)
+			{
+				kSize -= K_STEP*j;
+				variance = pow((kSize+2)/6, 2);
+				// We create the octogonal kernel 
+				lti::kernel2D<float> gKernel2D = filterController.GenerateSquareOddGaussianFilter(kSize,variance,true);
+
+				n = ( 1-( (n_actual*m_actual + kSize*kSize)/(N_MAX*M_MAX + K_MAX*K_MAX) ) ) * (NC_MAX - NC_MIN) + NC_MIN;
+
+				// Calculate the new image and filter to get the n data
+				for (int k = 0; k < n; k++)
+				{
+					lti::timer chron;
+					chron.start();
+		
+					// Funciones para tomar el tiempo
+					filterController.ConvolutionSquareFilter(gKernel2D,imgG);
+
+					chron.stop();
+					dataMatrix[i][j] =  (dataMatrix[i][j] + chron.getTime())/n;
+				}
+			}
+		}
+
+
+
+
+
+
+
 		
 		GenerateOctavePlot meshPlot = GenerateOctavePlot();
 		
